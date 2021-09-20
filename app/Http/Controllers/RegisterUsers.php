071003901e4c36d697347eebed3b304cb6210b5d
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2021-09-20 14:42:17 +0200

    Update

---
+++ app/Http/Controllers/RegisterUsers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\Users;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterUsers extends Controller
+{
+    public function index()
+    {
+        return view('register');
+    }
+
+    public function store(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+
+        Users::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+        return redirect()->route('register')->with('success', '');
+    }
+
+}
---
+++ app/Models/Users.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'password'
+    ];
+    public $timestamps = false;
+
+}
