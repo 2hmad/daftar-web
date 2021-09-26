382cfb4ca1d5571c1f61218f024560bb54ed2e40
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-09-27 01:57:24 +0200

    update

--- app/Http/Controllers/AddUsersController.php
+++ app/Http/Controllers/AddUsersController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class AddUsersController extends Controller
 {
     public function index()
     {
-        return view('add-users');
+        $table = DB::table('releated_users')
+        ->where('releated_user', '=', Session::get('email'))
+        ->get();
+        return view('add-users', compact('table'));
     }
     public function store() {
-        
+
     }
 }
